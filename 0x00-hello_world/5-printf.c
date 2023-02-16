f0b99c689801bc3afdf48bcc22b2a2d6596640a5
Boza28 <bb22bb82@gmail.com>
2023-02-16 06:17:37 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints "with proper grammar, but the outcome is 
- * a piece of art," followed by a new line. 
- * Return: Always 0.
+ * a piece of art,"
+ * Return:  0 (Success)
  */
 
 int main(void)
