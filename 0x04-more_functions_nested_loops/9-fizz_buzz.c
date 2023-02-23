168485afe7dd30385aa5d64452fda60824404b93
Boza28 <bb22bb82@gmail.com>
2023-02-23 04:11:39 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,10 +21,11 @@ int main(void)
 	{
 	printf(" Buzz");
 	}
-	 else if (i % 3 == 0 && i % 5 == 0)
+	else if (i % 3 == 0 && i % 5 == 0)
 	{
 	printf(" FizzBuzz");
-	} else if (i == 1)
+	}
+	else if (i == 1)
 	{
 	printf("%d", i);
 	}
