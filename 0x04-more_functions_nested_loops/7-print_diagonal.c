ca9b984095e7cad2dcfa1426e611f455f55fc3d5
Boza28 <bb22bb82@gmail.com>
2023-02-23 03:58:45 -0800

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,7 +9,8 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 	_putchar('\n');
-	} else
+	}
+	else
 	{
 	int i, j;
 
@@ -22,7 +23,7 @@ void print_diagonal(int n)
 	else if (j < i)
 	_putchar(' ');
 	}
-	_putchar('\n');
+	 _putchar('\n');
 	}
 	}
 }
